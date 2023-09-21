bb9d7e593c0a86e922033513a7b2bf9b33fe0510
“FernandaBarrosLinhares” <fernanda_linhares@estudante.sesisenai.org.br>
2023-09-21 14:53:35 -0300

    first commit

---
+++ src/test/java/br/lab365/Sete/SeteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.lab365.Sete;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
